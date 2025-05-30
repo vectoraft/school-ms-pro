5b7e56a439100e826c0cfd029eb899277b334c37
vectoraft <vectoraft@gmail.com>
2025-05-30 12:25:02 +0000

    Pending changes exported from your codespace

--- includes/class-shortcodes.php
+++ includes/class-shortcodes.php
@@ -2,6 +2,18 @@
 // Handles all plugin shortcodes and Elementor integration
 class School_MS_Pro_Shortcodes {
     public static function register_shortcodes() {
-        // Register all shortcodes for dashboards, login, etc.
+        // Dashboard shortcode
+        add_shortcode('schoolms_dashboard', function($atts) {
+            ob_start();
+            echo '<div id="school-ms-pro-root"></div>';
+            echo '<script src="/wp-content/plugins/school-ms-pro/assets/js/main.js"></script>';
+            return ob_get_clean();
+        });
+        // Login shortcode
+        add_shortcode('schoolms_login', function($atts) {
+            ob_start();
+            include plugin_dir_path(__FILE__) . '../templates/login.php';
+            return ob_get_clean();
+        });
     }
 }
